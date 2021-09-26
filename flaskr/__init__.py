8023b5e391a21b21b343574f10b603927359ac76
Shengpei Wang <shengpeiwang@gmail.com>
2021-09-26 19:22:10 -0400

    created html templates

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import (Flask, g, render_template, flash, redirect, request, session, url_for)
 from .make_drink import bar_tend
 
 import sys
@@ -31,7 +31,32 @@ def create_app(test_config=None):
     # a simple page that says hello
     @app.route('/bar')
     
-    def hello():
-        return bar_tend()
+    def bar():
+        flash(bar_tend(g.list1, g.list2, g.list3))
+        g.lucky_number = None
+        return render_template('base.html')
+
+    @app.route('/enter', methods=('GET', 'POST'))
+    def enter():
+        if request.method == 'POST':
+            g.lucky_number = request.form['Lucky Number']
+
+        return render_template('base.html')
+
+    @app.route('/ingredients')
+    def ingredients():
+        if not hasattr(g, 'list1'):
+            g.list1 = ["double", "your choice", "Rum", "Whiskey", "Wine", "Vodka"]
+        if not hasattr(g, 'list2'):
+            g.list2 = ["nothing to dilute it", "Coke", "Club soda", "milk", "heavy cream", 
+                       "Pineapple juice"]
+        if not hasattr(g, 'list3'):
+            g.list3 =["whatever evil the person to your right decides", "a dollop of honey", 
+                      "salted rim", "a splash of lime juice", "some hot sauce", 
+                      "a dash of Venilla"]
+        flash(g.list1[2:])
+        flash(g.list2[1:])
+        flash(g.list3)
+        return render_template('ingredients.html')
 
     return app
